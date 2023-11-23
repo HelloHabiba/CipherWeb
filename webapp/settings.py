3f656e9fb28ce834dcb0a5190002196690d0ce6b
XORbit <130087473+XORbit01@users.noreply.github.com>
2023-11-23 21:05:27 +0200

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-nb%w3)i-3(zm*hzy1d&bmy3)os0yr8$(10i1@7@b%fz=j1!u&f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://*.ciphey.me","https://*.habeeba.uk"]
+ALLOWED_HOSTS = ["ciphey.me","cipherweb.habeeba.uk"]
+CSRF_TRUSTED_ORIGINS = ['https://*.habeeba.uk','https://*.127.0.0.1','https://ciphey.me']
+
 
 
 # Application definition
