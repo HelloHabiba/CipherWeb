ab7a8c60734d0a9461fc604d128470d6b6f890eb
XORbit <aliawada127001@outlook.com>
2023-12-05 04:35:49 +0200

    acks late

--- webapp/settings.py
+++ webapp/settings.py
@@ -152,3 +152,4 @@ FILE_UPLOAD_HANDLERS = [
 CELERY = True
 CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/0') 
 CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_ACKS_LATE = True
